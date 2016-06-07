1634774ee7ad59f432836246bb05b884d6d9ed61
Ragavikaruppannan <k.ragavidevi@gmail.com>
2016-06-06 21:52:24 -0700

    Create postive.c

---
+++ postive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d",&num);
+	if(num>0)
+	{
+		printf("the number is negative");
+	}
+	else
+	{
+		printf("the number is postive");
+	}else if(num==0)
+	{
+		printf("the number is 0");
+	}
+}
